f8d0fef9b135c06cab4df888c0ee32da5599e5e4
Jatin Prajapati <jatinsprajapati@gmail.com>
2025-12-18 09:15:07 +0000

    Removed required tags,

--- CIC.API/DTO/ResponseDTO/AuthTokenResponse.cs
+++ CIC.API/DTO/ResponseDTO/AuthTokenResponse.cs
@@ -8,25 +8,25 @@ namespace CIC.API.DTO.ResponseDTO
         public string pa_webloginname { get; set; }
         [JsonProperty("emailaddress1")]
         public string Email { get; set; }
-        public required string contactid { get; set; }
+        public string contactid { get; set; }
         public bool pa_member { get; set; }
-        public required string pa_contactnumber { get; set; }
-        public required string fullname { get; set; }
-        public required string parentcustomerid_id { get; set; }
-        public required string parentcustomerid { get; set; }
-        public required string firstname { get; set; }
-        public required string defaultpricelevelid_id { get; set; }
-        public required string defaultpricelevelid { get; set; }
-        public required string pa_labelname { get; set; }
-        public required string lastname { get; set; }
-        public required string pa_token { get; set; }
+        public string pa_contactnumber { get; set; }
+        public string fullname { get; set; }
+        public string parentcustomerid_id { get; set; }
+        public string parentcustomerid { get; set; }
+        public string firstname { get; set; }
+        public string defaultpricelevelid_id { get; set; }
+        public string defaultpricelevelid { get; set; }
+        public string pa_labelname { get; set; }
+        public string lastname { get; set; }
+        public string pa_token { get; set; }
         public Account account { get; set; } = new Account();
-        public required List<PaWebRole> pa_webroles { get; set; }
+        public List<PaWebRole> pa_webroles { get; set; }
     }
 
     public class Account
     {
         public string cic_ipeds { get; set; }
-    }   
+    }
 
 }
